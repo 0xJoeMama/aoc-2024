3c9dae11a9ee806da13256a18bfe7d1bdde149fb
0xJoeMama <0xjoemama@gmail.com>
2024-12-01 13:20:07 +0200

    impr: switched to my utility crates

--- day-01/src/main.rs
+++ day-01/src/main.rs
@@ -1,35 +1,37 @@
 use std::collections::HashMap;
 
-const FILE: &str = include_str!("../input.txt");
+use aoc_companion_codegen::day;
+#[day(1, parser=parser, part1=part1, part2=part2)]
+const DAY: u32 = 1;
 
-fn main() {
-    let (mut left, mut right) = FILE
+fn parser(input: &str) -> (Vec<u32>, Vec<u32>) {
+    let (mut left, mut right) = input
         .lines()
         .map(|l| {
             let mut it = l.split_whitespace().flat_map(|s| s.parse::<u32>());
             (it.next().unwrap(), it.next().unwrap())
         })
-        .collect::<(Vec<u32>, Vec<u32>)>();
+        .collect::<(Vec<_>, Vec<_>)>();
 
     left.sort();
     right.sort();
+    (left, right)
+}
 
-    // part 1
-    let sum = left
-        .iter()
+fn part1((left, right): &(Vec<u32>, Vec<u32>)) -> u32 {
+    left.iter()
         .zip(right.iter())
         .map(|(left_n, right_n)| right_n.abs_diff(*left_n))
-        .sum::<u32>();
+        .sum::<u32>()
+}
 
+fn part2((left, right): &(Vec<u32>, Vec<u32>)) -> u32 {
     let freq_map: HashMap<u32, u32> = right.iter().fold(HashMap::new(), |mut acc, v| {
         *acc.entry(*v).or_insert(0) += 1;
         acc
     });
 
-    let new_sum = left
-        .iter()
+    left.iter()
         .filter_map(|left_v| freq_map.get(left_v).map(|it| it * left_v))
-        .sum::<u32>();
-
-    println!("{sum}, {new_sum}");
+        .sum::<u32>()
 }
