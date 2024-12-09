6d94563c7d6a5a183de55e17d7613917745d79bb
0xJoeMama <0xjoemama@gmail.com>
2024-12-09 19:59:48 +0200

    feat: day 9b

--- day-09/src/main.rs
+++ day-09/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::{hash_map::Entry, HashMap};
+use std::collections::{HashMap, HashSet};
 
 use aoc_companion_codegen::day;
 
@@ -60,31 +60,76 @@ fn part1(input: &Input) -> usize {
     cksum
 }
 
+#[derive(Debug)]
 struct Bucket {
     curr_cap: u32,
-    files: Vec<usize>,
+    files: Vec<(usize, usize)>,
 }
 
 fn part2(input: &Input) -> usize {
-    let mut right_idx = input.len();
+    let mut right_idx = input.len() - 1;
+    // notice that our inputs are always odd
+    let mut right_file = input.len() / 2;
 
     let mut consume_map: HashMap<usize, Bucket> = HashMap::with_capacity(input.len() / 2);
+    let mut removed = HashSet::with_capacity(input.len() / 2);
 
     while right_idx > 0 {
-        // get all empty positions from the beginning
+        if right_idx % 2 == 1 {
+            right_idx -= 1;
+            continue;
+        }
+
+        // get all spaces from the beginning
         for i in (1..right_idx).step_by(2) {
-            let bucket = consume_map.entry(i).or_insert(Bucket {
+            let bucket = consume_map.entry(i).or_insert_with(|| Bucket {
                 curr_cap: input[i],
-                files: Vec::new(),
+                files: Vec::with_capacity(10),
             });
-            if bucket.curr_cap > input[right_idx] {
-                bucket.files.push(right_idx);
+
+            if bucket.curr_cap >= input[right_idx] {
+                bucket.files.push((right_idx, right_file));
+                removed.insert(right_idx);
                 bucket.curr_cap -= input[right_idx];
+                break;
             }
         }
 
-        right_idx -= 2;
+        right_idx -= 1;
+        right_file -= 1;
     }
 
-    todo!()
+    let mut cksum = 0;
+    let mut idx = 0usize;
+    let mut left_file = 0;
+    for i in 0..input.len() {
+        if let Some(bucket) = consume_map.get(&i) {
+            for (right_idx, right_file) in &bucket.files {
+                for _ in 0..input[*right_idx] {
+                    cksum += idx * right_file;
+                    idx += 1;
+                }
+            }
+            idx += bucket.curr_cap as usize;
+            continue;
+        }
+
+        if i % 2 == 1 {
+            idx += input[i] as usize;
+            continue;
+        }
+
+        if !removed.contains(&i) {
+            for _ in 0..input[i] {
+                cksum += left_file * idx;
+                idx += 1;
+            }
+            left_file += 1;
+        } else {
+            idx += input[i] as usize;
+            left_file += 1;
+        }
+    }
+
+    cksum
 }
